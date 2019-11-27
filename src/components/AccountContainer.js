a8c29e8f0f7e8ae112739cc8423c9fd0413b76c9
Israel <Israel.pro1994@gmail.com>
2019-11-27 10:45:00 -0600

    done

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -4,23 +4,49 @@ import CategorySelector from './CategorySelector'
 import {transactions} from '../transactionsData'
 
 class AccountContainer extends Component {
-  constructor() {
-    super()
-    //... your code here
+
+  // constructor() {
+  //   super()
+  //   //... your code here
+  // }
+
+  state={
+    transactions: transactions,
+    filteredList: []
   }
 
-  handleChange() {
-    //... your code here
+  componentDidMount(){
+    fetch('https://boiling-brook-94902.herokuapp.com/transactions')
+    .then( resp => resp.json() )
+    .then( transactionsData => 
+      this.setState({
+        transactions: transactionsData
+      }))
   }
 
+  handleChanged = (category) => {
+    // console.log(category)
+    let filteredArray = this.state.transactions.filter( transaction => transaction.category === category )
+    this.setState({
+      filteredList: filteredArray
+    })
+    // console.log('we filtered!', this.state.filteredList)
+    // console.log('does this pass through?')
+  }
+
+  
   render() {
-    console.log(transactions)
+    console.log(this.state.transactions)
+    console.log(this.state.filteredList)
     return (
       <div className="ui grid container">
 
-        <CategorySelector />
+        <CategorySelector handleChanged={this.handleChanged}/>
 
-        <TransactionsList />
+        <TransactionsList 
+        transactions={this.state.filteredList.length > 0 ? this.state.filteredList : this.state.transactions }
+        
+        />
 
       </div>
     )
